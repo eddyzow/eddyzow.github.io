ded6fdca52a4c184518d9a1ba2b1f65d47575011
eddyzow <45926119+eddyzow@users.noreply.github.com>
2024-06-23 17:33:23 -0400

    inserted homepage
    Homepage

---
+++ client.js
@@ -0,0 +1,39 @@
+particlesJS.load("particles-js", "particles.json");
+minipages = ["net", "about", "projects", "contact", "hacks"];
+
+[...document.getElementsByClassName("header-item")].forEach((el) => {
+  el.onclick = function () {
+    [...document.getElementsByClassName("header-item")].forEach((el2) => {
+      el2.classList.remove("selected");
+    });
+    el.classList.add("selected");
+    window.location.hash = minipages[el.id.split("header")[1] - 1];
+    document.getElementById("main-carrier").style.left =
+      "-" + (el.id.split("header")[1] * 100 - 100) + "vw";
+  };
+});
+
+window.onload = () => {
+  try {
+    console.log(minipages.indexOf(window.location.hash.split("#")[1]));
+    [...document.getElementsByClassName("header-item")].forEach((el2) => {
+      el2.classList.remove("selected");
+    });
+    document
+      .getElementById(
+        "header" + (minipages.indexOf(window.location.hash.split("#")[1]) + 1)
+      )
+      .classList.add("selected");
+    document.getElementById("main-carrier").style.left =
+      "-" +
+      ((minipages.indexOf(window.location.hash.split("#")[1]) + 1) * 100 -
+        100) +
+      "vw";
+  } catch {
+    window.location.hash = "net";
+    [...document.getElementsByClassName("header-item")].forEach((el2) => {
+      el2.classList.remove("selected");
+    });
+    document.getElementById("header1").classList.add("selected");
+  }
+};
